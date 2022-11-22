a9b12277ef1a68cc807b099f134a3e955eb2bc37
Boris Vorberg <grebrov@gmx.de>
2022-11-22 13:34:18 +0100

    Update __init__.py

--- luiginotebook/__init__.py
+++ luiginotebook/__init__.py
@@ -2,9 +2,5 @@ import time
 import luigi
 import requests
 import pandas
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-from time import gmtime, strftime, strptime
-from datetime import datetime
 
 
