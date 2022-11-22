4472d0f7cb67c77ad3aa92574e17b5d0620bd986
Boris Vorberg <grebrov@gmx.de>
2022-11-22 14:32:41 +0100

    Update __init__.py

--- luiginotebook/__init__.py
+++ luiginotebook/__init__.py
@@ -6,4 +6,4 @@ import pandas
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from time import gmtime, strftime, strptime
-import .lnbutils
+import lnbutils
