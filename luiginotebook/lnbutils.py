ed0078b646a43b1c98e662637f712b9299f44c53
Boris Vorberg <grebrov@gmx.de>
2022-11-22 14:52:32 +0100

    Update lnbutils.py

--- luiginotebook/lnbutils.py
+++ luiginotebook/lnbutils.py
@@ -1,20 +1,12 @@
 import time
-#import luigi
-import requests
-import pandas as pd
-import pandas
+from luigi import Target, configuration
+from requests import Session 
+from pandas import DataFrame
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from time import gmtime, strftime, strptime
 
-def fibo(n):    # write Fibonacci series up to n
-    a, b = 0, 1
-    while a < n:
-        print(a, end=' ')
-        a, b = b, a+b
-    print()
-
-#luigi.configuration.get_config().set('core', 'default_scheduler_host', 'luigid-s')
+configuration.get_config().set('core', 'default_scheduler_host', 'luigid-s')
 
 def startZeppelinNotebook(id):
     #todo: get id by name,
@@ -57,7 +49,7 @@ def getZeppelinNotebookStatus(id, time):
     if response.status_code == 200:
         response_json = response.json()
         #print(response_json)
-        dfStatus=pd.DataFrame.from_dict(response_json['body']['paragraphs'])
+        dfStatus=DataFrame.from_dict(response_json['body']['paragraphs'])
         dfStatus.dropna(subset=['status', 'finished'], inplace=True)
         startTimes=dfStatus['started'].apply(lambda x: strptime(x,'%a %b %d %H:%M:%S %Z %Y'))
         lastStartTime=startTimes.agg(max)
@@ -75,18 +67,18 @@ def getZeppelinNotebookStatus(id, time):
     #print("Status: " + status + " lastStartTime: " +strftime('%d %m %Y %H:%M:%S',lastStartTime) + " time: " +strftime('%d %m %Y %H:%M:%S',time.timetuple()))
     return status, dfStatus
     
-#class ZeppelinNotebookTarget(luigi.Target):
-#    """
-#    This target checks if the notebook executed successfully.
-#    """
-#
-#    def __init__(self, id, time):
-#        self.host = "zeppelin-server"
-#        self.port = "80"
-#        self.id = id
-#        self.time=time
-#
-#    def exists(self):
-#        print("in exists")
-#        status, dfStatus=getZeppelinNotebookStatus(self.id,self.time)
-#        return (status=="FINISHED")
+class ZeppelinNotebookTarget(Target):
+    """
+    This target checks if the notebook executed successfully.
+    """
+
+    def __init__(self, id, time):
+        self.host = "zeppelin-server"
+        self.port = "80"
+        self.id = id
+        self.time=time
+
+    def exists(self):
+        print("in exists")
+        status, dfStatus=getZeppelinNotebookStatus(self.id,self.time)
+        return (status=="FINISHED")
