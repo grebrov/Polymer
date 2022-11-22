84e84fec1fba88da8f0135fea76542df932849e3
Boris Vorberg <grebrov@gmx.de>
2022-11-22 13:18:56 +0100

    Update lnbutils.py

--- luiginotebook/lnbutils.py
+++ luiginotebook/lnbutils.py
@@ -1,12 +1,3 @@
-import time
-import luigi
-import requests
-import pandas as pd
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-from time import gmtime, strftime, strptime
-from datetime import datetime
-
 luigi.configuration.get_config().set('core', 'default_scheduler_host', 'luigid-s')
 
 def startZeppelinNotebook(id):
